29dbaf48636fece20f217928365a3f6ae8f04c65
imukeshkumarprajapa <imukeshkumarprajapat@gmail.com>
2025-11-12 13:18:03 +0530

    model trainer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 from src.ml_first_project.components.data_ingestion import DataIngestion
 from src.ml_first_project.components.data_ingestion import DataIngestionConfig
 from src.ml_first_project.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.ml_first_project.components.model_trainer import MedelTrainer, ModelTrainerConfig
 
 if __name__ == "__main__":
     logging.info("Starting the ML First Project application...")
@@ -15,7 +16,18 @@ if __name__ == "__main__":
        train_data_path, test_data_path=data_ingestion.initiate_data_ingestion()
        #data_transformation_config=DataTransformationConfig()
        data_transformation=DataTransformation()
-       data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+       train_arr, test_arr,preprocessor_path=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+       #model Training 
+
+       model_trainer=MedelTrainer()
+       print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
+
+
+
+
 
     except Exception as e:
         logging.info("custom exception raised")
---
+++ src/ml_first_project/components/model_trainer.py
@@ -0,0 +1,128 @@
+import os
+import sys
+from dataclasses import dataclass
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import pickle
+import numpy as np
+from sklearn.metrics import mean_squared_error,mean_absolute_error
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.ml_first_project.exception import CustomException
+from src.ml_first_project.logger import logging
+from src.ml_first_project.utils import save_object
+
+from src.ml_first_project.utils import evaluate_model
+# model_trainer.py me:
+
+
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path: str = os.path.join('artifacts', 'model.pkl')
+
+class MedelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            
+            logging.info("split training and test input data")
+            X_train,y_train, X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+
+            )
+            models={
+                 "Random Forest":RandomForestRegressor(),
+                 "DecisionTreeRegressor":DecisionTreeRegressor(),
+                 "GradientBoostingRegressor":GradientBoostingRegressor(),
+                 "LinearRegression":LinearRegression(),
+                 "XGBRegressor":XGBRegressor(),
+                 "CatBoostRegressor":CatBoostRegressor(verbose=False),
+                 "AdaBoostRegressor":AdaBoostRegressor(),
+            }
+            params={
+                "DecisionTreeRegressor": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoostingRegressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoostRegressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            model_report:dict=evaluate_model(X_train, y_train, X_test,y_test, models, params)
+            #to get best model score from dict
+            best_model_score=max(sorted(model_report.values()))
+
+            #to get best model name from dict
+
+           # best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            
+            best_model=models[best_model_name]
+
+            print("this is the best model:")
+
+            print(best_model_name)
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(X_test)
+            r2=r2_score(y_test, predicted)
+            return r2
+            
+
+        except Exception as e:
+          raise CustomException(e, sys)
